786cd15275351f81019894c7fc8fe57af925df14
Nikhil Reddy <nikhil.cvr@gmail.com>
2017-12-01 10:24:34 -0500

    Update Column09.py

--- Part-1/Column09.py
+++ Part-1/Column09.py
@@ -12,45 +12,45 @@ from datetime import datetime
 tests = [
    #(Type, Test)
     (int, int),
-    (float, float),
     (str,str),
+    (float, float),
     (datetime, lambda value: datetime.strptime(value, "%Y/%m/%d"))
 ]
 
-def getValidEntries(value):
-    pattern = re.compile('^\d{5}(?:[-\s]\d{4})?$')
-    if (pattern.match(str(value))):
+def getValidEntries(val):
+    patt = re.compile('^\d{5}(?:[-\s]\d{4})?$')
+    if (patt.match(str(val))):
         return True
     else:
         return False
 
-def getDataType(x):
+def getDataType(key):
     label="invalid"
     for typ, test in tests:
         try:
-            test(x)
+            test(key)
             typ=str(typ).strip('<type').strip('>').strip(' ').strip('\'')
-            myVal = bool(getValidEntries(x))
-            if (typ=="int" or typ=="str") and myVal:
+            myFlag = bool(getValidEntries(key))
+            if (typ=="int" or typ=="str") and myFlag:
                 label="valid"
             val=typ+","+"ZipCode,"+label
 
-            return x,val
+            return key,val
         except ValueError:
             continue
-     #No match
+     #Not a match
     val="str,ZipCode,invalid"
     return x,val
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: IncidentZip.py <file>")
+        print("Usage: column-09.py <file>")
         exit(-1)
     sc = SparkContext()
     csvfile = sc.textFile(sys.argv[1],1)
     unique_key = csvfile.mapPartitions(lambda x: csv.reader(x)).map(lambda x: x[8])
     base_type=unique_key.map(lambda x: getDataType(x))
     base_type.saveAsTextFile("column-09.txt")
-    #base_type.collect()
+    
     sc.stop()
