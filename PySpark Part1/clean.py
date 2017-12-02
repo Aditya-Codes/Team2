ab6746c8a7db2eada411d2d82750f96c5147b790
Nikhil Reddy <nikhil.cvr@gmail.com>
2017-12-02 00:17:11 -0500

    Update clean.py

--- PySpark Part1/clean.py
+++ PySpark Part1/clean.py
@@ -8,4 +8,33 @@ from pyspark.sql.functions import count,length,col
 
 df = sql_c.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/user/sdv267/new_311.csv')
 
+#Finding invalid Zip Codes
 df.where(length(col('Incident Zip')) > 0).select(col('Incident Zip')).filter(col('Incident Zip').rlike('^\d{5}(?:[-\s]\d{4})?$')!=True).groupBy('Incident Zip').count().show()
+'''
++------------+-----+
+|Incident Zip|count|
++------------+-----+
+|     UNKNOWN|    1|
+|          NA|   12|
+|       113??|    1|
+|         N/A|   31|
+|      000000|    1|
+|           ?|    1|
+|           X|    1|
+|   402901921|    1|
+|        0000|    1|
+|   070545020|    1|
+|      198884|    1|
+|         103|    1|
++------------+-----+
+'''
+#Finding Unspecified Borough entries
+df.where(col('Borough').isin("BRONX","BROOKLYN","MANHATTAN","STATEN ISLAND","QUEENS")!=True).groupBy('Borough').count().show()
+'''
++-----------+------+
+|    Borough| count|
++-----------+------+
+|Unspecified|224127|
++-----------+------+
+'''
+
