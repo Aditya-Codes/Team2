6b4a350f1087b44ae7ffaf31dc03863620cdfb58
Nikhil Reddy <nikhil.cvr@gmail.com>
2017-12-03 18:31:02 -0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -9,7 +9,7 @@ from pyspark.sql.functions import count,length,col,when,isnan,udf
 
 df = sql_c.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/user/jzm218/new_311.csv')
 
-
+# dropping columns which are not necessary and donot contain much information as majority entries were null.
 drop_list = ['Landmark',
 			'Facility Type', 
 			'School Name', 
@@ -33,11 +33,10 @@ drop_list = ['Landmark',
 			'Latitude',
 			'Longitude']
 
-
-
-
 df = df.select([name for name in df.schema.names if name not in drop_list])
 
+
+#Replacing Null values with "N/A"
 for x in df.schema.names:
 	# Basic replacement 
 	df = df.withColumn(x, when(col(x).isin("", "Unspecified", "0 Unspecified") != True, col(x)).otherwise('N/A'))  
@@ -45,46 +44,7 @@ for x in df.schema.names:
 # NA Statistics
 df.select([count(when(col(c).isin('N/A'), c)).alias(c) for c in df.columns]).show()
 
-df.write.format('com.databricks.spark.csv').options(header='true').save('cleaned_311.csv') 
-
-'''
-df.fillna(0)
-
-#def validAgency(x):
-#	return when(~ (col(x) != "3-1-1") | col(x).rlike('\\b[A-Z]{3}\\b|\\b[A-Z]{4}\\b|\\b[A-Z]{5}\\b') == True, #col(x)).otherwise(None)
-	
-new_column_udf = udf(lambda name: None if name == "" | (name != "3-1-1" & ~col(name).rlike('\\b[A-Z]{3}\\b|\\b[A-Z]{4}\\b|\\b[A-Z]{5}\\b')) else name, StringType())
-	
-df = df.withColumn("Agency", new_column_udf(df["Agency"])) # Cleaning agency column
+df.write.format('com.databricks.spark.csv').options(header='true').save('/user/sdv267/cleaned_311.csv') 
 
 
-df.cllect()
-
-df.select(count(when(isnan("Agency") | col("Agency").isNull(), "Agency")).alias("Agency")).show()
-
-
-'''
-
-#Finding invalid Zip Codes
-#df.where(length(col('Incident Zip')) > 0).select(col('Incident Zip')).filter(col('Incident Zip').rlike('^\d{5}(?:[-\s]\d{4})?$')!=True).groupBy('Incident Zip').count().show()
-
-#df = df.withColumn('Incident Zip', when(col('Incident Zip').rlike('^\d{5}(?:[-\s]\d{4})?$')!= True, 'N/A').otherwise(df['Incident Zip']))
-
-#Finding Unspecified Borough entries
-#df.where(col('Borough').isin("BRONX","BROOKLYN","MANHATTAN","STATEN ISLAND","QUEENS")!=True).groupBy('Borough').count().show()
-
-
-#Finding invalid Agency Acronyms.
-#df.where(length(col('Agency')) > 0).select(col('Agency')).filter(col('Agency').rlike('\\b[A-Z]{3}\\b|\\b[A-Z]{4}\\b|\\b[A-Z]{5}\\b')!=True).groupBy('Agency').count().show()
-# found that the invalid ones are 3-1-1 which is a valid agency acronym. Hence no outliers.
-
-#Finding invalid Complaint Descriptors
-#df.where(length(col('Descriptor')) > 0).select(col('Descriptor')).filter(col('Descriptor').rlike('^(?:[A-Z]|[a-z]|[0-9]|&|/|\s|\(|\)|,|\+|\.|"|-|\:)+$')!=True).groupBy('Descriptor').count().show()
-#found no invalid entries
-        
-#Finding invalid 
-#df.where(length(col('Community Board')) > 0).select(col('Community Board')).filter(col('Community Board').rlike("^(?:[A-Za-z0-9 ])+$")!=True).groupBy('Community Board').count().show()
-#found no invalid entries
-
-    
-#df.write.format('com.databricks.spark.csv').save('cleaned_311.csv')                                           
+                                         
