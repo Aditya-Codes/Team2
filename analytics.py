6451ba4dc9fee783e2e3fb5cea9891ef737e1283
Nikhil Reddy <nikhil.cvr@gmail.com>
2017-12-16 00:53:19 -0500

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -8,4 +8,26 @@ from pyspark.sql.functions import count,length,col,when,isnan
 
 df = sql_c.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('/user/sdv267/cleaned_*.csv')
 
+# Number of complaints per Borough
 df.groupBy('Borough').count().show()
+
+#Number of complaints per year
+df = df.withColumn('year', col('Created Date').substr(7,4))
+df.groupBy('year').count().show()
+'''
++----+-------+                                                                  
+|year|  count|
++----+-------+
+|2009| 489607|
+|2010|2005760|
+|2011|1918896|
+|2012|1783212|
+|2013|1849019|
+|2014|2102226|
+|2015|2286951|
+|2016|2370339|
+|2017|2238425|
++----+-------+
+'''
+
+
