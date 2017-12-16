e52f5371a39a2047c145a59b541d83964d18aa4e
JohnMartinez12 <jzm218@nyu.edu>
2017-12-16 12:12:51 -0500

    Update _analytics.py

--- _analytics.py
+++ _analytics.py
@@ -4,8 +4,19 @@
 
 
 '''
-# This is how you group complaints. 
+# Noise Group
 df = df.withColumn(x, when(col(x).like('%Noise%') != True, col(x)).otherwise('NOISE'))
+
+# Street Group
 df = df.withColumn(x, when(col(x).like('%Street%') != True, col(x)).otherwise('STREET'))
-df = df.withColumn(x, when(col(x).like('%HEAT%') != True, col(x)).otherwise('HEAT'))
+
+# Heat Group
+df = df.withColumn(x, when(col(x).isin('HEAT/HOT WATER','HEATING', 'Non-Residential Heat', 'Sidewalk Cafe Heater') != True, col(x)).otherwise('HEAT'))
+
+# Water group
+df = df.withColumn(x, when(col(x).like('%Water%') != True, col(x)).otherwise('WATER'))
+
+# Construction Group
+df = df.withColumn(x, when(col(x).isin('GENERAL CONSTRUCTION', 'General Construction/Plumbing', 'Construction', 'CONSTRUCTION') != True, col(x)).otherwise('CONSTRUCTION'))
+
 '''
