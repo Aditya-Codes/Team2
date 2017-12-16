cec7f0e718156c406bcad5777eae5c45d3e4b955
JohnMartinez12 <jzm218@nyu.edu>
2017-12-16 11:57:48 -0500

    Update _analytics.py

--- _analytics.py
+++ _analytics.py
@@ -1,3 +1,11 @@
 
 
 #df = df.filter(df['Created Date'].like('%2009%')) # This is how you filter the date by year! 
+
+
+'''
+# This is how you group complaints. 
+df = df.withColumn(x, when(col(x).like('%Noise%') != True, col(x)).otherwise('NOISE'))
+df = df.withColumn(x, when(col(x).like('%Street%') != True, col(x)).otherwise('STREET'))
+df = df.withColumn(x, when(col(x).like('%HEAT%') != True, col(x)).otherwise('HEAT'))
+'''
